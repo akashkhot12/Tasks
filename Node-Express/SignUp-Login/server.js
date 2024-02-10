a711c4f6a47ce054aac71005ff2e71fbfb2e4f57
akashkhot12 <akashkhot03@gmail.com>
2024-02-10 09:22:19 +0530

    create a server

---
+++ Node-Express/SignUp-Login/server.js
@@ -0,0 +1,12 @@
+const express  = require("express");
+const app = express();
+
+
+app.get((req,res)=>{
+    res.send("<h1>hello your server is on </h1>")
+    res.end();
+})
+
+app.listen(5000,()=>{
+    console.log("server is up go server ");
+})
