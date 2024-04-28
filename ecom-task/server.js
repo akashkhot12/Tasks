1e3b9116346c1b8b08ae8da889f3b20cf3f27746
akashkhot12 <akashkhot03@gmail.com>
2024-04-28 11:57:04 +0530

    adding node modules

---
+++ ecom-task/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const path  = require('path');
+const bodyParser = require('body-parser')
+const session = require("express-session");
+
+const {v4:uuidv4}=require("uuid")
+// const router = require('./router')
+
+const app = express();
+
+app.set("view engine",'ejs');
+
+// load static assets
+app.use('/static',express.static(path.join(__dirname,'public')))
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(session({
+    secret:uuidv4(),
+    resave:false,
+    saveUninitialized:true
+}));
+
+// app.use('/route',router)
+
+
+
+// home route
+app.get('/',(req,res)=>{
+   res.render('ecomHomePage',{title:"Ecom Site"})
+})
+
+app.listen(5000,()=>{
+    console.log("server is up on 5000 port");
+})
