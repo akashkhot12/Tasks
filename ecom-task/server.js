2eec663292a79fccd4ef857d9288ac995b520807
akashkhot12 <akashkhot03@gmail.com>
2024-04-28 13:19:00 +0530

    adding json data

--- ecom-task/server.js
+++ ecom-task/server.js
@@ -3,6 +3,7 @@ const path  = require('path');
 const bodyParser = require('body-parser')
 const session = require("express-session");
 const productData = require('./products.json')
+const fs = require('fs')
 
 const {v4:uuidv4}=require("uuid")
 // const router = require('./router')
@@ -24,13 +25,22 @@ app.use(session({
 
 // app.use('/route',router)
 
-
-
-// home route
-app.get('/',(req,res)=>{
-   res.render('ecomHomePage',{title:"Ecom Site"},{jsonData: productData})
-})
+// // home route
+app.get('/', (req, res) => {
+    // Read JSON data from file
+    fs.readFile('products.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error('Error reading file:', err);
+            return res.status(500).send('Error reading file');
+        }
+        // Parse JSON data
+        const jsonData = JSON.parse(data);
+        // Render the EJS template with JSON data
+        res.render('ecomHomePage', { jsonData: jsonData });
+    });
+});
 
 app.listen(5000,()=>{
     console.log("server is up on 5000 port");
 })
+
